8dcd95dc2313b5044b4ef26d96f1ff77ec1ac541
Yevhenii Hrzhenda <jonleev@gmail.com>
2023-01-30 21:04:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<script setup>
+  import {ref} from "vue";
+
+  const showModal = ref(false);
+  const newNote = ref("");
+  const notes = ref([]);
+
+</script>
+
+<template>
+  <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <button>Add Note</button>
+        <button @click="showModal=false" class="close">Close</button>
+      </div>
+    </div>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button @click="showModal = !showModal">+</button>
+      </header>
+      <div class="card-container">
+        <div class="card">
+          <p class="main-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, dolor dolore maiores similique expedita vel?</p>
+          <p class="date">2023-01-25</p>
+        </div>
+        <div class="card">
+          <p class="main-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, dolor dolore maiores similique expedita vel?</p>
+          <p class="date">2023-01-25</p>
+        </div>        
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  h1 {
+    font-weight: bold;
+    margin-bottom: 25px;
+    font-size: 60px;
+  }
+
+  header button {
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: rgb(21,20,20);
+    border-radius: 100%;
+    color: #ffffff;
+    font-size: 20px;
+  }
+
+  .card {
+    width: 225px;
+    height: 225px;
+    background-color: rgb(237, 182, 44);
+    padding: 10px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+
+  .date {
+    font-size: 12.5px;
+    font-weight: bold;
+  }
+
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.77);
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .modal {
+    width: 750px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    padding: 30px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+  .modal button {
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 100%;
+    background-color: blueviolet;
+    border: none;
+    color: #ffffff;
+    cursor: pointer;
+    margin-top: 15px;
+  }
+
+  .modal .close {
+    background-color: crimson;
+    margin-top: 10px;
+  }
+</style>
